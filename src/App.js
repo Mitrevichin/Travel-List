791c56b361c680677c1a869dbdfeeec290540962
Petar Mitrevichin <pmitrevichin@gmail.com>
2024-02-29 14:45:23 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+export default function App() {
+  return (
+    
+  );
+}
+
+function Logo() {
+  return (
+    <h1>🌴 Far away 💼</h1>
+  );
+}
+
+function Form() { }
+
+function PackingList() { }
+
+function Stats() { }
